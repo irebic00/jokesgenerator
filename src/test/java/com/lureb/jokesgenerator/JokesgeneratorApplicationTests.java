ebedd32acdc13f1bbe1c60a279834354679cedc6
VrebiC <irebic@outlook.com>
2020-08-23 17:09:21 +0200

    Initial commit

---
+++ src/test/java/com/lureb/jokesgenerator/JokesgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lureb.jokesgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
